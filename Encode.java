38e40ab06ca87e8bd2ff404f120fe66e46014480
sharanka <sharanka96@hotmail.com>
2018-05-21 15:24:21 +0200

    Comments on Encode

--- Encode.java
+++ Encode.java
@@ -5,8 +5,8 @@ import java.util.logging.Logger;
 
 public class Encode {
 
-    private int[] bytes = new int[256];
-    private String[] keywords = new String[256];
+    private int[] bytes = new int[256]; //Entries that contains frequencies for every single byte
+    private String[] keywords = new String[256]; //Content of the entries.
     private String fileName;
     private FileInputStream fileInput;
     private FileOutputStream fileOutput;
@@ -18,6 +18,7 @@ public class Encode {
     public static void main(String[] args) {
         Encode e = new Encode();
         if (args.length > 1) {
+            //Name of the files, that are used to encode and decode
             e.setFileName(args[0]);
             e.setEncodedFileName(args[1]);
         } else {
@@ -25,37 +26,38 @@ public class Encode {
         }
         try {
             System.out.println("before scan input");
-            e.scanInput();
-        } catch (IOException ex) {
+            e.scanInput(); //Scanning the input file 
+        } catch (IOException ex) { 
             Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
         }
-        e.generateKeyWords(e.huffman(e.getBytes()));
+        e.generateKeyWords(e.huffman(e.getBytes())); //Bytes from the entries are converted into keywords
         try {
-            e.toFile(e.keywords);
+            e.toFile(e.keywords); //The encoded occurances and every byte's keyword are written into the file. 
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
-
+    /*
+     * Scan input file using FileInputStream  
+     */
     public void scanInput() throws FileNotFoundException, IOException {
-        for (int i = 0; i < getBytes().length; i++) {
-            bytes[i] = 0;
+        for (int i = 0; i < getBytes().length; i++) { //looping through entries 
+            bytes[i] = 0; //Makes sure every indexes contains something in the entry array
         }
         int _byte;
         try {
-            fileInput = new FileInputStream(new File(fileName));
+            fileInput = new FileInputStream(new File(fileName)); 
 
-            while ((_byte = fileInput.read()) != -1) {
-                getBytes()[_byte]++;
-                //System.out.println(getBytes()[_byte]);
+            while ((_byte = fileInput.read()) != -1) { //Reads input of the entry array
+                getBytes()[_byte]++; //Increments the index place in the entry array
             }
         } catch (IOException ioe) {
             System.out.println(ioe);
         } finally {
             try {
-                if (fileInput != null) {
-                    fileInput.close();
+                if (fileInput != null) { //Check if input file is not null
+                    fileInput.close(); //, and closes FileInputStream if that is the case
                 }
 
             } catch (IOException ioe) {
@@ -64,99 +66,84 @@ public class Encode {
         }
         System.out.println("end scan input");
     }
-
+    /*
+     * Convert content from bytes to keywords
+     */
     public void generateKeyWords(Element e) {
         HuffManTree ht = (HuffManTree) e.data;
-        for (int i = 0; i < keywords.length; i++) {
-            convert(ht.getRoot(), i, "");
+        for (int i = 0; i < keywords.length; i++) { //Loops through the content of every entries
+            convert(ht.getRoot(), i, ""); //Convert from byte to keyword
         }
     }
-
+    /*
+     * Convert from Huffman's tree to table
+     */
     public String convert(Node current, Integer _byte, String path) {
-        if (current != null) {
-            if (current.getLeftChild() != null) {
-                path = path.concat("0");
-                //System.out.println("added 0 total path is: " + path);
-                path = convert(current.getLeftChild(), _byte, path);
+        if (current != null) { //If current Node is not null
+            if (current.getLeftChild() != null) { //And if left child
+                path = path.concat("0"); //add 0 to the path.
+                path = convert(current.getLeftChild(), _byte, path); //convert left child 
             }
-            if (current.getRightChild() != null) {
-                path = path.concat("1");
-                //System.out.println("added 1 total path is: " + path);
-                path = convert(current.getRightChild(), _byte, path);
+            if (current.getRightChild() != null) { //If right child
+                path = path.concat("1"); //add 1 to the path.
+                path = convert(current.getRightChild(), _byte, path); //convert right child
             }
-            if (current.get_byte() == _byte) {
-                keywords[_byte] = path;
+            if (current.get_byte() == _byte) { //get the current byte
+                keywords[_byte] = path; //add the byte to keywords to the path
                 System.out.println("path is: " + path);
             }
         }
-        if (path.length() > 0) {
-            //System.out.println("removing last bit");
-            path = path.substring(0, path.length() - 1);
-            //System.out.println("path is: " + path);
+        if (path.length() > 0) { //checks if length of the path is greater than 0
+            path = path.substring(0, path.length() - 1); //removes a character from the path
         }
         return path;
     }
-
-    private void buildPQHeap(PQHeap pqHeap, int[] bytes) {
+    /*
+     * Insert entires to HuffmanTree
+    */
+    private void buildPQHeap(PQHeap pqHeap, int[] bytes) { 
         int n = bytes.length;
         for (int i = 0; i < n; i++) {
-            //System.out.println(bytes[i]);
-            pqHeap.insert(new Element(bytes[i], new HuffManTree(new Node(i))));
+            pqHeap.insert(new Element(bytes[i], new HuffManTree(new Node(i)))); //Using insert from PQHeap to add the entries to the Huffman tree as a Node
         }
     }
 
-    /*public Element huffman(int[] _bytes) {
-        int n = _bytes.length;
-        pqHeap = new PQHeap(n);
-        buildPQHeap(pqHeap, _bytes);
-        for (int i = 1; i < n; i++) {
-            Node z = new Node();
-            HuffManTree ht = (HuffManTree) pqHeap.extractMin().data;
-            z.setLeftChild(ht.getRoot());
-            ht = (HuffManTree) pqHeap.extractMin().data;
-            z.setRightChild(ht.getRoot());
-            HuffManTree data = new HuffManTree(z);
-            System.out.println("left child byte frequency" + _bytes[z.getLeftChild().get_byte()]);
-            System.out.println("right child byte frequency" + _bytes[z.getRightChild().get_byte()]);
-            Element element = new Element(_bytes[z.getLeftChild().get_byte()] + _bytes[z.getRightChild().get_byte()], data);
-            pqHeap.insert(element);
-        }
-        return pqHeap.extractMin();
-    }*/
-
+    /*
+     * HuffMan algorithm
+    */
     public Element huffman(int[] _bytes) {
         System.out.println("enter huffman");
         int n = _bytes.length;
         pqHeap = new PQHeap(n);
         buildPQHeap(pqHeap, _bytes);
         for (int i = 1; i < n; i++) {
-            Node z = new Node();
-            Element x = pqHeap.extractMin();
-            Element y = pqHeap.extractMin();
-            HuffManTree hx = (HuffManTree) x.data;
-            HuffManTree hy = (HuffManTree) y.data;
+            Node z = new Node(); 
+            Element x = pqHeap.extractMin(); //Extracts first element
+            Element y = pqHeap.extractMin(); //Extracts the new first element
+            HuffManTree hx = (HuffManTree) x.data; //Add the element to the Huffman tree
+            HuffManTree hy = (HuffManTree) y.data; //Add the new first element to the Huffmann tree
 
-            z.setLeftChild(hx.getRoot());
-            z.setRightChild(hy.getRoot());
-            HuffManTree data = new HuffManTree(z);
-            Element element = new Element(x.key + y.key, data);
-            pqHeap.insert(element);
+            z.setLeftChild(hx.getRoot()); //The first element of the Huffmann tree is a root of the tree
+            z.setRightChild(hy.getRoot()); //The new first element of the Huffman tree is a root of the tree
+            HuffManTree data = new HuffManTree(z); //Create new trees
+            Element element = new Element(x.key + y.key, data); //Merge two trees
+            pqHeap.insert(element); //Insert that element to the tree
         }
         System.out.println("exit huffman");
         return pqHeap.extractMin();
     }
+    /*
+     * Write all the occurances and every bytes keyword
+    */
 
     public void toFile(String[] passwords) throws FileNotFoundException {
-
-        //Write all the occurances
-        //write every bytes keyword
         fileOutput = new FileOutputStream(new File(encodedFileName));
         bitOutput = new BitOutputStream(fileOutput);
         fileInput = new FileInputStream(new File(fileName));
 
-        for (int i = 0; i < getBytes().length; i++) {
+        for (int i = 0; i < getBytes().length; i++) { //Loop through the entry array
             try {
-                bitOutput.writeInt(getBytes()[i]);
+                bitOutput.writeInt(getBytes()[i]); //Output is written to the file
             } catch (IOException ex) {
                 Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -165,15 +152,13 @@ public class Encode {
         int _byte;
         try {
 
-            while ((_byte = fileInput.read()) != -1) {
-                //System.out.println("byte frequency is: " + _byte);
-                //System.out.println("keywords at _byte is: " + keywords[_byte]);
+            while ((_byte = fileInput.read()) != -1) { //Read the compressed file 
                 String string = keywords[_byte];
-                if (string != "" && string != null) {
-                    if (string.length() <= 9) {
-                        bitOutput.writeInt(Integer.parseInt(keywords[_byte]));
-                    } else {
-                        bitOutput.writeInt(Integer.parseInt(keywords[_byte].substring(0,9)));
+                if (string != "" && string != null) { //Check if the content of keywords in the entry array is null 
+                    if (string.length() <= 9) { //Check if keyword's length is less than 9
+                        bitOutput.writeInt(Integer.parseInt(keywords[_byte])); //, and write the int value to the output stream
+                    } else { //if the length of keyword is greater than 9, the string is split into two 
+                        bitOutput.writeInt(Integer.parseInt(keywords[_byte].substring(0,9))); 
                         bitOutput.writeInt(Integer.parseInt(string.substring(9, string.length())));
                     }
                 }
@@ -182,10 +167,9 @@ public class Encode {
             System.out.println(ioe);
         } finally {
             try {
-                if (fileInput != null) {
-                    fileInput.close();
+                if (fileInput != null) { //Check if input file is null
+                    fileInput.close(); // if not null, the input file is closed
                 }
-
             } catch (IOException ioe) {
                 System.out.println(ioe);
             }
